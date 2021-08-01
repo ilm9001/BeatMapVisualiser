4d0b5863321695555fbe9c6c5f7f567196a31c7c
ilm9001 <ilmari100@hotmail.fi>
2021-08-01 19:37:05 +0300

    add autocomplete to /playbeatmap

--- src/main/java/com/ilm9001/beatmapvisualiser/BeatMapVisualiser.java
+++ src/main/java/com/ilm9001/beatmapvisualiser/BeatMapVisualiser.java
@@ -1,5 +1,6 @@
 package com.ilm9001.beatmapvisualiser;
 
+import com.ilm9001.beatmapvisualiser.Commands.PlayCommandTabComplete;
 import com.ilm9001.beatmapvisualiser.LE.LE_list;
 
 import org.bukkit.ChatColor;
@@ -47,8 +48,9 @@ public final class BeatMapVisualiser extends JavaPlugin {
             show = new Show(bmv, stg);
         }
 
-        // create the play command hook
-        Objects.requireNonNull(this.getCommand("playbeatmap")).setExecutor(new PlayCommand());
+        // create the play command hook + autocompleter
+        this.getCommand("playbeatmap").setExecutor(new PlayCommand());
+        this.getCommand("playbeatmap").setTabCompleter(new PlayCommandTabComplete());
     }
 
     @Override
@@ -60,114 +62,116 @@ public final class BeatMapVisualiser extends JavaPlugin {
     public class PlayCommand implements CommandExecutor {
         @Override
         public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, String[] args) {
-            Player pl = (Player)sender;
-            final String bfile = args[0];
-
-            if (bfile.contains("/")) {
-                pl.sendMessage(ChatColor.RED + "Folder name must not contain /");
-                return false;
-            }
-
-            // get specified path
-            String df = getDataFolder().getAbsolutePath();
-            String bfile_a = df + "/" + bfile + "/" + bfile + ".dat";
-
-            String info = df + "/" + bfile + "/" + "info.dat";
-
-            JSONParser jsonParser = new JSONParser();
-            JSONObject json_o;
-            JSONObject json_info;
-
-            // parse beatmap from path
-            try {
-                FileReader reader = new FileReader(bfile_a);
-                FileReader inforeader = new FileReader(info);
-
-                json_o = (JSONObject)jsonParser.parse(reader);
-                json_info = (JSONObject)jsonParser.parse(inforeader);
-            } catch (IOException e) {
-                pl.sendMessage(ChatColor.RED + "Could not read beatmap: " + bfile);
-                e.printStackTrace();
-                return false;
-            } catch (ParseException e) {
-                pl.sendMessage(ChatColor.RED + "Could not parse beatmap: " + bfile);
-                e.printStackTrace();
-                return false;
-            }
-            if (json_o == null) {
-                pl.sendMessage(ChatColor.RED + "Invalid beatmap: no json?.");
-                return false;
-            }
-            JSONArray json_ev_list = (JSONArray)json_o.get("_events");
-            Object bpm = json_info.get("_beatsPerMinute");
-
-            Object artistName = json_info.get("_songAuthorName");
-            Object songName = json_info.get("_songName");
-
-            Object mapperName = json_info.get("_levelAuthorName");
-
-            double bpmVal;
-            if(bpm instanceof Long) {
-                bpmVal = ((Long)bpm).doubleValue();
-            } else if (bpm instanceof Double) {
-                bpmVal = (Double) bpm;
+            if(sender.hasPermission("beatmapvisualiser.playbeatmap")) {
+                Player pl = (Player) sender;
+                final String bfile = args[0];
+    
+                if (bfile.contains("/")) {
+                    pl.sendMessage(ChatColor.RED + "Folder name must not contain /");
+                    return false;
+                }
+    
+                // get specified path
+                String df = getDataFolder().getAbsolutePath();
+                String bfile_a = df + "/" + bfile + "/" + bfile + ".dat";
+    
+                String info = df + "/" + bfile + "/" + "info.dat";
+    
+                JSONParser jsonParser = new JSONParser();
+                JSONObject json_o;
+                JSONObject json_info;
+    
+                // parse beatmap from path
+                try {
+                    FileReader reader = new FileReader(bfile_a);
+                    FileReader inforeader = new FileReader(info);
+        
+                    json_o = (JSONObject) jsonParser.parse(reader);
+                    json_info = (JSONObject) jsonParser.parse(inforeader);
+                } catch (IOException e) {
+                    pl.sendMessage(ChatColor.RED + "Could not read beatmap: " + bfile);
+                    e.printStackTrace();
+                    return false;
+                } catch (ParseException e) {
+                    pl.sendMessage(ChatColor.RED + "Could not parse beatmap: " + bfile);
+                    e.printStackTrace();
+                    return false;
+                }
+                if (json_o == null) {
+                    pl.sendMessage(ChatColor.RED + "Invalid beatmap: no json?.");
+                    return false;
+                }
+                JSONArray json_ev_list = (JSONArray) json_o.get("_events");
+                Object bpm = json_info.get("_beatsPerMinute");
+    
+                Object artistName = json_info.get("_songAuthorName");
+                Object songName = json_info.get("_songName");
+    
+                Object mapperName = json_info.get("_levelAuthorName");
+    
+                double bpmVal;
+                if (bpm instanceof Long) {
+                    bpmVal = ((Long) bpm).doubleValue();
+                } else if (bpm instanceof Double) {
+                    bpmVal = (Double) bpm;
+                } else {
+                    bpmVal = (double) bpm;
+                }
+    
+                if (json_ev_list == null) {
+                    pl.sendMessage(ChatColor.RED + "Invalid beatmap: no events.");
+                    return false;
+                }
+                // Iterate over event array, call method parseEvent for each element on list
+                LE_list le_list = new LE_list();
+                //noinspection unchecked
+                json_ev_list.forEach(eve -> le_list.add((JSONObject) eve, (int) bpmVal));
+    
+                final int le_size = le_list.size();
+                long last_time = 0;
+                int failcount = 0;
+                int n_ev = 0;
+                // We check that time of events is always increasing.
+                for (int i = 0; i < le_size; ++i) {
+                    ++n_ev;
+                    long t = le_list.get(i).time_ms;
+                    if (t >= last_time) {
+                        last_time = t;
+                    } else {
+                        ++failcount;
+                    }
+                }
+                bmv.getLogger().info(String.format("Beatmap %s bpm %f len %d, failcount: %d, last time: %.1f s",
+                        bfile_a, bpmVal, le_size, failcount, last_time / 1000.0));
+    
+                if (n_ev == 0) {
+                    bmv.getLogger().info("Beatmap with zero events?");
+                    pl.sendMessage(ChatColor.RED + "beatmap with zero events?");
+                    return false;
+                }
+                if (failcount > 0) {
+                    // Somewhere in the beatmap time runs backwards.
+                    bmv.getLogger().info("Beatmap is invalid, time runs backwards somewhere in it.");
+                    pl.sendMessage(ChatColor.RED + "Backward time flow detected in beatmap: " + bfile);
+                    return false;
+                }
+    
+                // New scheduling method!
+                if (show.Run(le_list, last_time / 1000.0)) {
+                    Player[] players = getServer().getOnlinePlayers().toArray(new Player[0]);
+                    for (Player plr : players) {
+                        plr.playSound(plr.getLocation(), "minecraft:" + bfile, 1, 1);
+                        //theres probably a much better way to send these to the players
+                        plr.sendMessage(ChatColor.DARK_GRAY + "[" + ChatColor.GOLD + "BMVPlayer" + ChatColor.DARK_GRAY + "] " + ChatColor.WHITE + "Now playing " + ChatColor.AQUA + artistName + ChatColor.WHITE + " - " + ChatColor.AQUA + songName);
+                        plr.sendMessage(ChatColor.DARK_GRAY + "[" + ChatColor.GOLD + "BMVPlayer" + ChatColor.DARK_GRAY + "] " + ChatColor.WHITE + "Mapped by " + ChatColor.AQUA + mapperName);
+                    }
+                    return true;
+                } else {
+                    bmv.getLogger().info("Beatmap playback active, cannot start new one.");
+                    pl.sendMessage(ChatColor.RED + "Beatmap playback active, cannot start new one.");
+                    return false;
+                }
             } else {
-                bpmVal = (double) bpm;
-            }
-
-            if (json_ev_list == null) {
-                pl.sendMessage(ChatColor.RED + "Invalid beatmap: no events.");
-                return false;
-            }
-            // Iterate over event array, call method parseEvent for each element on list
-            LE_list le_list = new LE_list();
-            //noinspection unchecked
-            json_ev_list.forEach(eve -> le_list.add((JSONObject)eve, (int) bpmVal));
-
-            final int le_size = le_list.size();
-            long last_time = 0;
-            int failcount = 0;
-            int n_ev = 0;
-            // We check that time of events is always increasing.
-            for (int i=0; i < le_size; ++i) {
-                ++n_ev;
-                long t = le_list.get(i).time_ms;
-                if (t >= last_time) {
-                    last_time = t;
-                }
-                else {
-                    ++failcount;
-                }
-            }
-            bmv.getLogger().info(String.format("Beatmap %s bpm %f len %d, failcount: %d, last time: %.1f s",
-                    bfile_a, bpmVal, le_size, failcount, last_time/1000.0));
-
-            if (n_ev == 0) {
-                bmv.getLogger().info("Beatmap with zero events?");
-                pl.sendMessage(ChatColor.RED + "beatmap with zero events?");
-                return false;
-            }
-            if (failcount > 0) {
-                // Somewhere in the beatmap time runs backwards.
-                bmv.getLogger().info("Beatmap is invalid, time runs backwards somewhere in it.");
-                pl.sendMessage(ChatColor.RED + "Backward time flow detected in beatmap: " + bfile);
-                return false;
-            }
-
-            // New scheduling method!
-            if (show.Run(le_list,last_time/1000.0)) {
-                Player[] players = getServer().getOnlinePlayers().toArray(new Player[0]);
-                for (Player plr : players) {
-                    plr.playSound(plr.getLocation(),"minecraft:"+bfile,1,1);
-                    //theres probably a much better way to send these to the players
-                    plr.sendMessage(ChatColor.DARK_GRAY + "[" + ChatColor.GOLD + "BMVPlayer" + ChatColor.DARK_GRAY + "] " + ChatColor.WHITE + "Now playing " + ChatColor.AQUA + artistName + ChatColor.WHITE + " - " + ChatColor.AQUA + songName);
-                    plr.sendMessage(ChatColor.DARK_GRAY + "[" + ChatColor.GOLD + "BMVPlayer" + ChatColor.DARK_GRAY + "] " + ChatColor.WHITE + "Mapped by " + ChatColor.AQUA + mapperName);
-                }
-                return true;
-            }
-            else {
-                bmv.getLogger().info("Beatmap playback active, cannot start new one.");
-                pl.sendMessage(ChatColor.RED + "Beatmap playback active, cannot start new one.");
                 return false;
             }
         }
---
+++ src/main/java/com/ilm9001/beatmapvisualiser/Commands/PlayCommandTabComplete.java
@@ -0,0 +1,29 @@
+package com.ilm9001.beatmapvisualiser.Commands;
+
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.io.File;
+import java.util.*;
+//Autocomplete class for /playbeatmap
+public class PlayCommandTabComplete implements TabCompleter {
+   
+   @Nullable
+   @Override
+   public List<String> onTabComplete(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String[] strings) {
+      if(commandSender.hasPermission("beatmapvisualiser.playbeatmap")) {
+         String df = Bukkit.getPluginManager().getPlugin("BeatMapVisualiser").getDataFolder().getAbsolutePath();
+         File[] directories = new File(df).listFiles(File::isDirectory); //adds all subdirectories in plugin datafolder path to a File array
+         List<String> list = new ArrayList<String>();
+         for (File file : directories) {
+            list.add(file.getName());
+         }
+         return list;
+      }
+      return null;
+   }
+}
